be2c8694fc31f9288d77144139b943081565e99e
Jason Lau <39978937+khlauaj@users.noreply.github.com>
2023-04-24 21:51:43 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+const API_KEY = " "; // replace with your actual API key
+
+async function fetchData() {
+  const response = await fetch("https://api.openai.com/v1/completions", {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${API_KEY}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      model: "text-davinci-003",
+      prompt: "hello, how are you today?",
+      max_tokens: 7,
+      temperature: 1,
+    }),
+  });
+  const data = await response.json();
+  console.log(data);
+}
+
+fetchData();
